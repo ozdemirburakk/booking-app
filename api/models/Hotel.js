0f2352d9ca7ebff5cd89b913a7be5e25c915aa4c
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2022-06-28 12:35:00 +0300

    Hotelschema

--- api/models/Hotel.js
+++ api/models/Hotel.js
@@ -1,9 +1,40 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const HotelSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  photos: {
+    type: [String],
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  rooms: {
+    type: [String],
+  },
+});
