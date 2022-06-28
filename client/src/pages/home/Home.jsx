2b436c495b26ed195f3054d35a4e61ca3cb1aab2
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2022-06-28 15:42:48 +0300

    featured-list-comp

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.css";
@@ -7,6 +8,10 @@ const Home = () => {
     <>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+      </div>
     </>
   );
 };
