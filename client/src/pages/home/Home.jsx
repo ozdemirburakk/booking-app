8cf467b1e1b5be7024f9beb88f42edccd0b792de
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2022-06-28 14:40:52 +0300

    navbar-and-header-components

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,14 @@
+import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.css";
 
 const Home = () => {
-  return <div><Navbar /></div>;
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
 };
 
 export default Home;
