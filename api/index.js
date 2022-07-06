989c4328ab55ec06386f7477fe3663c3d2e8924e
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2022-07-06 11:24:55 +0300

    error-and-controller

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import authRoute from "./routes/auth.js"
-import usersRoute from "./routes/users.js"
-import hotelsRoute from "./routes/hotels.js"
-import roomsRoute from "./routes/rooms.js"
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 const app = express();
 dotenv.config();
@@ -26,18 +26,25 @@ mongoose.connection.on("connected", () => {
   console.log("MongoDB connected!");
 });
 
-
 //middlewares
+
 app.use(express.json());
 
-app.use("/api/auth",authRoute);
-app.use("/api/users",authRoute);
-app.use("/api/hotels",hotelsRoute);
-app.use("/api/rooms",authRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
-app.use((req,res,next)=>{
-  console.log("hi im a middleware")
-})
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status:errorStatus,
+    message:errorMessage,
+    stack:err.stack
+  });
+});
 
 app.listen(8800, () => {
   connect();
