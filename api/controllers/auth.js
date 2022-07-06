f128b80a89a7570538b1d834149dc994bf741033
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2022-07-06 12:08:53 +0300

    jwt and password hash user auth

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 //REGISTER
 export const register = async (req, res, next) => {
@@ -34,9 +35,18 @@ export const login = async (req, res, next) => {
     if (!isPasswordCorrect)
       return next(createError(400, "Wrong password or username!"));
 
-      const {password,isAdmin,...otherDetails} = user._doc 
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
 
-    res.status(200).json({...otherDetails});
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ ...otherDetails });
   } catch (err) {
     next(err);
   }
