6c84cf33f8896aee6848ab137f8accb2c89a48ed
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2022-08-16 12:19:31 +0300

    Update verifyToken.js

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -1,9 +1,11 @@
-import jwt from 'jsonwebtoken';
-import {createError} from '../utils/error.js'
+import jwt from "jsonwebtoken";
+import { createError } from "../utils/error.js";
 
-export const verifyToken = (req,res,next) =>{
-    const token = req.cookies.access_token;
-    if(!token){
-        return next(createError(401, "You are not authenticated!"))
-    }
-}
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(createError(401, "You are not authenticated!"));
+  }
+
+  jwt.verify(token, process.env.JWT, (err, user));
+};
